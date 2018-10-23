4bf019496d64d831d57b191363a1facc001c044c
ifoche <nacho.foche@gmail.com>
2018-10-23 09:22:03 +0200

    ignore existing folders

--- create_repo_structure.sh
+++ create_repo_structure.sh
@@ -7,11 +7,15 @@ echo "creating folders..."
 while IFS='|' read -r isocode countryname
 do
 	echo "iso code: $isocode - country name: $countryname"
-	mkdir $isocode
-	ret=$?
-	if [ $ret -ne 0 ]; then
-		echo "ERROR while creating repo structure. Please verify and fix it by hand..."
-		exit 1
+	if [ -d $isocode ]; then
+		echo "WARN folder $isocode already exist. Ignored..."
+	else
+		mkdir $isocode
+		ret=$?
+		if [ $ret -ne 0 ]; then
+			echo "ERROR while creating repo structure. Please verify and fix it by hand..."
+			exit 1
+		fi
 	fi
 done < "$file"
 
